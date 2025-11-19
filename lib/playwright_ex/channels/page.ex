fce674d29ad6ea6e82f95cd07893111c762360d4
Fredrik Teschke <f@ftes.de>
2025-11-19 15:04:56 +0100

    Add Page types

--- lib/playwright_ex/channels/page.ex
+++ lib/playwright_ex/channels/page.ex
@@ -11,21 +11,65 @@ defmodule PlaywrightEx.Page do
   alias PlaywrightEx.ChannelResponse
   alias PlaywrightEx.Connection
 
+  schema =
+    NimbleOptions.new!(
+      timeout: PlaywrightEx.Channel.timeout_opt(),
+      event: [type: :atom, required: true],
+      enabled: [type: :boolean, default: true]
+    )
+
+  @doc """
+  Updates the subscription for page events.
+
+  Reference: https://github.com/microsoft/playwright/blob/main/packages/playwright-core/src/client/page.ts
+
+  ## Options
+  #{NimbleOptions.docs(schema)}
+  """
+  @schema schema
+  @type update_subscription_opt :: unquote(NimbleOptions.option_typespec(schema))
+  @spec update_subscription(PlaywrightEx.guid(), [update_subscription_opt() | {Keyword.key(), any()}]) ::
+          {:ok, any()} | {:error, any()}
   def update_subscription(page_id, opts \\ []) do
+    {timeout, opts} = opts |> PlaywrightEx.Channel.validate_known!(@schema) |> Keyword.pop!(:timeout)
+
     %{guid: page_id, method: :update_subscription, params: Map.new(opts)}
-    |> Connection.send(opts[:timeout])
+    |> Connection.send(timeout)
     |> ChannelResponse.unwrap(& &1)
   end
 
-  def screenshot(page_id, opts \\ []) do
-    # Playwright options: https://playwright.dev/docs/api/class-page#page-screenshot
-    params =
-      opts
-      |> Keyword.validate!([:timeout, full_page: true, omit_background: false])
-      |> Map.new()
+  schema =
+    NimbleOptions.new!(
+      timeout: PlaywrightEx.Channel.timeout_opt(),
+      full_page: [
+        type: :boolean,
+        doc:
+          "When true, takes a screenshot of the full scrollable page, instead of the currently visible viewport. Defaults to `false`."
+      ],
+      omit_background: [
+        type: :boolean,
+        doc:
+          "Hides default white background and allows capturing screenshots with transparency. Defaults to `false`. Not applicable to jpeg images."
+      ]
+    )
 
-    %{guid: page_id, method: :screenshot, params: params}
-    |> Connection.send(opts[:timeout])
+  @doc """
+  Returns a screenshot of the page as binary data.
+
+  Reference: https://playwright.dev/docs/api/class-page#page-screenshot
+
+  ## Options
+  #{NimbleOptions.docs(schema)}
+  """
+  @schema schema
+  @type screenshot_opt :: unquote(NimbleOptions.option_typespec(schema))
+  @spec screenshot(PlaywrightEx.guid(), [screenshot_opt() | {Keyword.key(), any()}]) ::
+          {:ok, binary()} | {:error, any()}
+  def screenshot(page_id, opts \\ []) do
+    {timeout, opts} = opts |> PlaywrightEx.Channel.validate_known!(@schema) |> Keyword.pop!(:timeout)
+
+    %{guid: page_id, method: :screenshot, params: Map.new(opts)}
+    |> Connection.send(timeout)
     |> ChannelResponse.unwrap(& &1.binary)
   end
 end
--- lib/playwright_ex/periphery/channel.ex
+++ lib/playwright_ex/periphery/channel.ex
@@ -2,8 +2,16 @@ defmodule PlaywrightEx.Channel do
   @moduledoc false
   def timeout_opt, do: [type: :timeout, required: true, doc: "Maximum time for the operation (milliseconds)."]
 
-  def validate_known!(opts, schema) do
-    {known, unknown} = Keyword.split(opts, Keyword.keys(schema.schema))
-    NimbleOptions.validate!(known, schema) ++ unknown
+  @debug_unknown true
+
+  if @debug_unknown do
+    def validate_known!(opts, schema) do
+      NimbleOptions.validate!(opts, schema)
+    end
+  else
+    def validate_known!(opts, schema) do
+      {known, unknown} = Keyword.split(opts, Keyword.keys(schema.schema))
+      NimbleOptions.validate!(known, schema) ++ unknown
+    end
   end
 end
